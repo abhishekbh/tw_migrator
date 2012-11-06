cb4bd7cdae91ac3283b4a220683bbf84be839e7f
abhishekbh <abhishekbh@gmail.com>
2012-11-06 12:58:49 -0800

    output some stuff

--- index.js
+++ index.js
@@ -4,8 +4,11 @@ exports.migrate = function(req, res){
   var csv = require('csv');
 
   csv()
-    .from.stream(fs.createReadStream('sample.in'))
-    .to.path(__dirname+'/../sample.out')
+    .from.stream(
+      fs.createReadStream('iran.csv'), {
+      columns: ['Topic','Location','Tags']
+    })
+    .to.path('parsed.csv')
 //    .transform( function(data){
 //       data.unshift(data.pop());
 //       return data;
@@ -19,10 +22,6 @@ exports.migrate = function(req, res){
     .on('error', function(error){
       console.log(error.message);
     });
-// Output:
-// #0 ["2000-01-01","20322051544","1979.0","8.8017226E7","ABC","45"]
-// #1 ["2050-11-27","28392898392","1974.0","8.8392926E7","DEF","23"]
-// Number of lines: 2
 
   res.render('index', {
       title: 'The MigratOr'
