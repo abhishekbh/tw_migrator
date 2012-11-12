cff63e23248f4bc614d70d268ecaeda101484cc7
abhishekbh <abhishekbh@gmail.com>
2012-11-11 22:46:27 -0800

    new way, still no luck

--- index.js
+++ index.js
@@ -2,34 +2,51 @@ fs = require("fs");
 
 exports.migrate = function(req, res){
   var csv = require('csv');
-  tags = ['hello'];
-  status = false;
+  var local_tags = new tags('fake');
+  var status = false;
 
-  function doCSV(tags, callback) {
-    console.log();
+  function tags (tag) {
+    var tags = [];
+    tags.push(tag);
+ 
+    this.getValue = function(){
+        return tags;
+    };
+ 
+    this.setValue = function(tag){
+        tags.push(tag);
+    };
+  }
+
+  function doCSV(callback) {
     csv()
       .from.stream(
         fs.createReadStream('iran.csv')
+        ,{columns: ['Tags']}
       )
       .to.path(
         'tags.out.csv'
         ,{columns: ['Tags']}
       )
       .on('record', function(data, index){
-        tags.push(JSON.stringify(data));
-        console.log(tags.length);
+        local_tags.setValue(JSON.stringify(data));
+        console.log(JSON.stringify(data));
       })
       .on('error', function(error){
         console.log(error.message);
       });
+      console.log('1');
       callback();
-      return tags;
+      console.log('3');
+      console.log(local_tags.getValue());
+      return local_tags.getValue();
   }
 
   res.render('index', {
       title: 'The MigratOr'
-      , tags: doCSV(tags, function(){
+      , tags: doCSV(function(){
                 status = true;
+                console.log('2');
               })
       , status: status
   });
