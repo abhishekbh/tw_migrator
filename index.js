2792e1fd692f006105e72b033bc526bd1f9238f0
abhishekbh <abhishekbh@gmail.com>
2012-11-11 17:35:12 -0800

    ugh, stupid jade

--- index.js
+++ index.js
@@ -10,23 +10,19 @@ exports.migrate = function(req, res){
     csv()
       .from.stream(
         fs.createReadStream('iran.csv')
-        //,{columns: ['Tags']}
       )
       .to.path(
         'tags.out.csv'
         ,{columns: ['Tags']}
       )
       .on('record', function(data, index){
-        //console.log('#'+index+' '+JSON.stringify(data));
         tags.push(JSON.stringify(data));
         console.log(tags.length);
       })
-      //.on('end', function(count){
-        //console.log('Number of lines: '+count);
-      //})
       .on('error', function(error){
         console.log(error.message);
       });
+      callback();
       return tags;
   }
 
