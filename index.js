84ac98636902cae13f35d9c4972129c09e07d3af
abhishekbh <abhishekbh@gmail.com>
2012-11-11 16:58:25 -0800

    sync problem

--- index.js
+++ index.js
@@ -2,28 +2,34 @@ fs = require("fs");
 
 exports.migrate = function(req, res){
   var csv = require('csv');
+  tags = ['hello'];
 
-  csv()
-    .from.stream(
-      fs.createReadStream('iran.csv'), {
-      columns: ['Topic','Location','Tags']
-    })
-    .to.path('parsed.csv')
-//    .transform( function(data){
-//       data.unshift(data.pop());
-//       return data;
-//    })
-    .on('record', function(data,index){
-      console.log('#'+index+' '+JSON.stringify(data));
-    })
-    .on('end', function(count){
-      console.log('Number of lines: '+count);
-    })
-    .on('error', function(error){
-      console.log(error.message);
-    });
+  function doCSV(tags) {
+    console.log();
+    csv(tags)
+      .from.stream(
+        fs.createReadStream('iran.csv')
+        //,{columns: ['Tags']}
+      )
+      .to.path(
+        'tags.out.csv'
+        ,{columns: ['Tags']}
+      )
+      .on('record', function(data, index){
+        //console.log('#'+index+' '+JSON.stringify(data));
+        tags.push(JSON.stringify(data));
+        console.log(tags.length);
+      })
+      //.on('end', function(count){
+        //console.log('Number of lines: '+count);
+      //})
+      .on('error', function(error){
+        console.log(error.message);
+      });
+  }
 
   res.render('index', {
       title: 'The MigratOr'
+      , tags: doCSV(tags)
   });
 };
